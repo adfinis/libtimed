e617a193e27de0f15004fa7bb773cbd26ed3cfa6
Gian Klug <gian.klug@adfinis.com>
2023-07-21 14:46:32 +0200

    fix: Use cache folder for caching

--- src/libtimed/__init__.py
+++ src/libtimed/__init__.py
@@ -13,7 +13,9 @@ class TimedAPIClient:
         self.session = requests.Session()
         self.session.headers["Authorization"] = f"Bearer {token}"
         self.session.headers["Content-Type"] = "application/vnd.api+json"
-        self.cached_session = requests_cache.CachedSession(expire_after=60 * 60 * 24)
+        self.cached_session = requests_cache.CachedSession(
+            "libtimed", expire_after=60 * 60 * 24, use_cache_dir=True
+        )
         self.cached_session.headers["Authorization"] = f"Bearer {token}"
         self.cached_session.headers["Content-Type"] = "application/vnd.api+json"
         # Models
